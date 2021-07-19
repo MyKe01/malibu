58c0a6a0ad4a5130d34c801ac5cf3f210e2194cc
f3ment <f3ment@protonmail.com>
2021-07-19 17:13:12 +0200

    created file /templates/body.php

---
+++ templates/body.php
@@ -0,0 +1,54 @@
+<!----------  BODY  ---------->
+<body>
+      <form action="sendmail.php" method ="post">
+        <div class="container">
+          <h2>Subscribe to our Newsletter</h2>
+          <p>Lorem ipsum..</p>
+        </div>
+      
+        <div class="container" style="background-color:white">
+          <input type="text" placeholder="Name" name="name" required>
+          <input type="text" placeholder="Email address" name="mail" required>
+          <label>
+            <input type="checkbox" checked="checked" name="subscribe"> Daily Newsletter
+          </label>
+        </div>
+      
+        <div class="container">
+          <input type="submit" value="Subscribe">
+        </div>
+      </form>
+
+      
+      
+      
+     
+  
+      <!----------  HEADER  ---------->
+      <header>
+        <div class="container-background">
+          <div class="opacita-header">
+            <div class="contenuto-header">
+              <h1 class="">HEADER</h1>
+            </div>
+          </div>
+        </div>
+      </header>
+      
+      <div class="spaziatore"></div>
+
+      
+  
+      <!----------  CONTENUTO  ---------->
+      <section class="content text-center">
+        <div class="container-immagine-contenuto">
+          
+        </div>
+      </section>
+     
+  
+      <!----------  JS Scripts Bootstrap  ---------->
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-p34f1UUtsS3wqzfto5wAAmdvj+osOnFyQFpp4Ua3gs/ZVWx6oOypYoCJhGGScy+8" crossorigin="anonymous"></script>
+      <!---------- Our Scripts  ---------->
+      <script src="src/script.js"></script>
+    </body>
