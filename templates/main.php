85b9a7972a934c9187ff9f9f5097126f19400b28
MyKe01 <mstasi01@gmail.com>
2021-07-23 22:08:43 +0200

    fixed form 2

--- templates/main.php
+++ templates/main.php
@@ -1,7 +1,5 @@
-    <p class="title">Coming Soon</p>
-    <div class="separator"></div>
-    <p class="message">Al momento stiamo lavorando al sito.</p>
-    <p class="message">Iscriviti alla newsletter per sapere quando sarà disponibile.</p>
-    <div class="separator"></div>
-
-    
+<p class="title">Coming Soon</p>
+<div class="separator"></div>
+<p class="message">Al momento stiamo lavorando al sito.</p>
+<p class="message">Iscriviti alla newsletter per sapere quando sarà disponibile.</p>
+<div class="separator"></div>
