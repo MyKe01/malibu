d7d1858e3db51318c84fa51f77673504bca938c8
f3ment <f3ment@protonmail.com>
2021-07-25 16:23:55 +0200

    Correzione .css ~ modifica alla struttura della repository

---
+++ Coming/Coming/templates/main.php
@@ -0,0 +1,5 @@
+    <p class="title">Coming Soon</p>
+    <div class="separator"></div>
+    <p class="message">Al momento stiamo lavorando al sito.</p>
+    <p class="message">Iscriviti per sapere quando sarà disponibile.</p>
+    
