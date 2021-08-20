623b968d3efe3356ec1a3e7939cdca2d82b5e97a
f3ment <f3ment@protonmail.com>
2021-08-20 18:17:52 +0200

    Rimuovo Bootstrap perchè intacca il foglio di stile

--- Coming/templates/header.php
+++ Coming/templates/header.php
@@ -4,6 +4,6 @@
     </section>
     <ul class="menu">
       <li><a href="/malibu/Coming/src/Pages/Contatti.php">Contatti</a></li>
-      <li><a href="/malibu/Coming/src/Pages/Il_Progetto.php">il progetto</a></li>
+      <li><a href="/malibu/Coming/src/Pages/Il_Progetto.php">progetto</a></li>
     </ul>
 </header>
