08457172571b3902beb87536bf5c6f4b234a4429
f3ment <f3ment@protonmail.com>
2021-08-20 17:32:18 +0200

    creo la pagina #Contatti

--- Coming/templates/header.php
+++ Coming/templates/header.php
@@ -3,7 +3,7 @@
       <a href="/malibu/Coming/">Modelist</a>
     </section>
     <ul class="menu">
-      <li><a href="#">Contatti</a></li>
+      <li><a href="/malibu/Coming/src/Pages/Contatti.php">Contatti</a></li>
       <li><a href="/malibu/Coming/src/Pages/Il_Progetto.php">il progetto</a></li>
     </ul>
 </header>
