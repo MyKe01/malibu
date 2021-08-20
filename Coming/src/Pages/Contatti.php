7023624dbca2086fe3c4dcbf72b4c03c6378fe27
f3ment <f3ment@protonmail.com>
2021-08-20 18:13:45 +0200

    Aggiungo Bootstrap

--- Coming/src/Pages/Contatti.php
+++ Coming/src/Pages/Contatti.php
@@ -15,10 +15,25 @@
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+  <!-- BOOTSTRAP -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+
 
 </head>
 <body>
-    prova prova prova
+  
+  
+  <header>
+    <section>
+      <a href="/malibu/Coming/">Modelist</a>
+    </section>
+    <ul class="menu">
+      <li><a href="#">Contatti</a></li>
+      <li><a href="/malibu/Coming/src/Pages/Il_Progetto.php">Il progetto</a></li>
+    </ul>
+  </header>
+  
+  
   <footer>
     <a href="#"><i class="fab fa-facebook-f"></i></a>
     <a href="#"><i class="fab fa-instagram"></i></a>
