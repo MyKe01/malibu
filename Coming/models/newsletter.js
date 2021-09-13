d57b9d2c0d8408ff7b88c1a85d03c02148476683
MyKe01 <mstasi01@gmail.com>
2021-09-13 23:17:34 +0200

    js version

---
+++ Coming/models/newsletter.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+//create newsletter schema & model
+const UserSchema = new Schema({
+    name : {
+        type : String,
+        required :[true, 'Name field is required']
+    },
+    email : {
+        type : String,
+        required :[true, 'Email field is required'],
+        unique : true
+    },
+});
+
+const User = mongoose.model('user', UserSchema);
+
+module.exports = User;
